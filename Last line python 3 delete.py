997034c87a80792e35659f9284b1a6cb6610e7b4
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2020-10-15 09:33:02 +0800

    Add files via upload

---
+++ Last line python 3 delete.py
@@ -0,0 +1,1 @@
+import os import sysimport time#last line deletiondef delete_last_line():    "Use this function to delete the last line in the STDOUT"    #cursor up one line    sys.stdout.write('\x1b[1A')    #delete last line    sys.stdout.write('\x1b[2K')        ###DEMO###print("this line will delete in 5 seconds")time.sleep(5)delete_last_line()
